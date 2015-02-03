689fdb187d8da17a4bc6f2b93e5058d381a1a85f
ManyCoding <vamukhs@gmail.com>
2015-02-02 18:54:08 -0800

    Added input dir check, formats' constants, pretty output

--- mlp.go
+++ mlp.go
@@ -1,92 +1,137 @@
 // TODO
-// / \
 // determine if correct album folder
 // custom names through arguments
-// check if input dir exist
-// extension in constants\array\enum
 // if multiple artwork in album folder
-// add statistics about removed files & scanned albums
+// / \
 
 package main
 
-import ( 
-		"os"
-		"path/filepath"
-		"strings"
-		"log"
-		"flag"
-		"fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
-const coverName = "folder"
-const artworkDirName = "artwork"
+const (
+	coverName      = "folder"
+	artworkDirName = "artwork"
+)
 
-// panic if any errors
-func check(e error) {
+var visitedDirCounter = 0
+var renamedFilesCounter = 0
+var audioFormats = []string{".m4a", ".flac"}
+var imageFormats = []string{".jpg", ".jpeg", ".png"}
+
+// errors handling
+func isError(e error, fatal bool) (isAnyError bool) {
 	if e != nil {
-		log.Panic(e)
+		if fatal {
+			log.Fatal(e)
+		} else {
+			log.Panic(e)
+		}
 	}
+	return e != nil
 }
 
-// check each file or directory
-func visit(path string, file os.FileInfo, err error) (e error) {
-	if file.IsDir() && !IsAlbumFolder(path) {
-		return filepath.SkipDir;
+// check if slice contains item
+func contains(slice []string, item string) bool {
+	set := make(map[string]struct{}, len(slice))
+	for _, s := range slice {
+		set[s] = struct{}{}
 	}
 
-	// format only artwork
-	if IsArtwork(filepath.Ext(path)) && !strings.HasPrefix(file.Name(), coverName) {
-		dir := filepath.Dir(path)
-		newName := filepath.Join(dir, coverName + filepath.Ext(path))
-		err := os.Rename(path, newName)
-		check(err)
-		fmt.Printf("Renamed \"%s\" >> \"%s\"\nin \"%s\"\n", filepath.Base(path), coverName + filepath.Ext(path), filepath.Base(filepath.Dir(path)))
+	_, ok := set[item]
+	return ok
+}
+
+// isError each file or directory
+func visit(path string, file os.FileInfo, err error) (e error) {
+	if file.IsDir() {
+		if IsAlbumFolder(path) {
+			visitedDirCounter++
+		} else {
+			return filepath.SkipDir
+		}
 	}
-	return	
+
+	// check if valid artwork
+	if !contains(imageFormats, filepath.Ext(path)) || strings.HasPrefix(file.Name(), coverName+".") {
+		return
+	}
+
+	dir := filepath.Dir(path)
+	newName := filepath.Join(dir, coverName+filepath.Ext(path))
+	err = os.Rename(path, newName)
+	if !isError(err, false) {
+		renamedFilesCounter++
+	}
+	fmt.Printf("Renamed \"%s\" >> \"%s\"\nin \"%s\"\n", filepath.Base(path), coverName+filepath.Ext(path), filepath.Base(filepath.Dir(path)))
+	return
 }
 
 // determine if album folder
-// is folder with music files and one arwork, or just folders
-func IsAlbumFolder(path string) (i bool) {	
-	files, _ := filepath.Glob(path + "/*.jp*g")
-	files2, _ := filepath.Glob(path + "/*.png")
+// is folder with music files and one artwork, or just folders
+func IsAlbumFolder(path string) bool {
+	var files []string
 
-	files = append(files, files2...)
-	
-	if (len(files) > 1) {
+	// get all files with extension from imageFormats
+	for _, imageFormat := range imageFormats {
+		filesBuff, _ := filepath.Glob(path + "/*" + imageFormat)
+		files = append(files, filesBuff...)
+	}
+
+	if len(files) > 1 {
 		return false
 	}
-	files, _ = filepath.Glob(path + "/*.m4a")
+
+	files = nil
+
+	// get all audio files
+	for _, audioFormat := range audioFormats {
+		filesBuff, _ := filepath.Glob(path + "/*" + audioFormat)
+		files = append(files, filesBuff...)
+	}
 
 	if files == nil {
 		dir, err := os.Open(path)
-		check(err)
+		isError(err, false)
 		defer dir.Close()
-		
+
 		files, err := dir.Readdir(-1)
-		check (err)
-		
+		isError(err, false)
+
 		for _, file := range files {
 			if file.Mode().IsDir() {
 				return true
 			}
-		}	
-	}		
+		}
+	}
 	return files != nil
-}
-
-func IsArtwork(ext string) (isArwork bool){
-	return ext == ".png" || ext == ".jpg" || ext == ".jpeg"
 }
 
 func main() {
 	flag.Parse()
-	
+
 	args := flag.Args()
-	// check input argument
+	// isError input argument
 	if len(args) != 1 {
-		log.Fatal("Only one argument (path to root) allowed")
+		log.Fatal("One argument (path to input directory) is compulsory")
 	}
+	fmt.Println("Input: " + args[0])
+
+	// isError if dir exist
+	src, err := os.Stat(args[0])
+	isError(err, true)
+
+	if !src.IsDir() {
+		log.Fatal(args[0] + " is not a directory")
+	}
+
 	// go traverse throught root, args[0] is path to root
 	filepath.Walk(args[0], visit)
+	fmt.Printf("\nfinished: %d album\u0028s\u0029 found, %d artwork\u0028s\u0029 renamed", visitedDirCounter, renamedFilesCounter)
 }
